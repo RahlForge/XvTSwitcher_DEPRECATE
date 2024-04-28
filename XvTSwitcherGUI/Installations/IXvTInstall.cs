51293613aacdc9f3d81bcd024aea6bc75dfafd3e
David Rahl <rahlforge@yahoo.com>
2024-04-27 21:36:51 -0400

    #1: Deleted associated XvTSwitcher project to move all files under XvTSwitcherGUI. Added MainWindow for GUI interface. Added label, textbox, and button for the Source Directory field. Button click opens a folder browser dialog to select a source folder, and the path is copied into the text field. On save, an installations.json is created to save the source directory as an XvTInstall object serialized to json. On open, if the file exists and has a value, that value is retrieved and fills the source directory field.

---
+++ XvTSwitcherGUI/Installations/IXvTInstall.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XvTSwitcherGUI.Installations
+{
+  public interface IXvTInstall
+  {
+    string Name { get; set; }
+    string Filepath { get; set; }
+  }
+}
---
+++ XvTSwitcherGUI/Installations/XvTInstall.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XvTSwitcherGUI.Installations
+{
+  public class XvTInstall : IXvTInstall
+  {
+    public string Name { get; set; }
+    public string Filepath { get; set; }
+
+    public XvTInstall(string name, string filepath)
+    {
+      Name = name;
+      Filepath = filepath;
+    }
+  }
+}
