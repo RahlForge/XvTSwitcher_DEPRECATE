b3a02245ca0366b5b8f7382b6a9ec1eefabab0d5
David Rahl <rahlforge@yahoo.com>
2024-04-29 00:42:21 -0400

    #3: Added functionality to store multiple installations to a List<XvTInstall>. This list is now serialized to the installations.json file, and deserialized on load. The NewInstall window has been resized and centered within the MainWindow as the Owner. If the Accept button is selected, the source directory is copied to a new Star Wars - XvT directory, formatted with the chosen name. Added the CopyDirectory.dll to avoid reproducing existing code.

--- XvTSwitcherGUI/Windows/MainWindow.xaml.cs
+++ XvTSwitcherGUI/Windows/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System.Windows;
-using System.Windows.Forms;
 using System.IO;
 using Newtonsoft.Json;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using XvTSwitcherGUI.Installations;
 using XvTSwitcherGUI.Windows;
+using System.Linq;
+using System.Windows.Input;
 
 namespace XvTSwitcherGUI
 {
@@ -13,44 +15,68 @@ namespace XvTSwitcherGUI
   /// </summary>
   public partial class MainWindow : Window
   {
+    private const string STAR_WARS_XVT = "Star Wars - XvT";
     private const string INSTALLATIONS_JSON = "installations.json";
     private const string BASE_GAME = "BaseGame";
 
     private List<XvTInstall> Installations = new List<XvTInstall>();
+    private XvTInstall BaseInstall => Installations.FirstOrDefault(o => o.Name == BASE_GAME);
+    private bool HasBaseInstall => BaseInstall != null;
 
     public MainWindow()
     {
       InitializeComponent();
 
       if (File.Exists(INSTALLATIONS_JSON) && new FileInfo(INSTALLATIONS_JSON).Length > 0)
-        SourceDirectory.Text = JsonConvert.DeserializeObject<XvTInstall>(File.ReadAllText(INSTALLATIONS_JSON)).Filepath;
+        Installations = JsonConvert.DeserializeObject<List<XvTInstall>>(File.ReadAllText(INSTALLATIONS_JSON));
+
+      SourceDirectory.Text = BaseInstall?.Filepath ?? string.Empty;
+      CreateNewInstall.IsEnabled = HasBaseInstall;
+    }
+
+    private void AddOrUpdateBaseInstallation()
+    {
+      if (HasBaseInstall)
+        BaseInstall.Filepath = SourceDirectory.Text;      
+      else
+        Installations.Add(new XvTInstall(BASE_GAME, SourceDirectory.Text));
     }
 
     private void BrowseSourceDirectory_Click(object sender, RoutedEventArgs e)
     {
-      var dlg = new FolderBrowserDialog();
-      DialogResult result = dlg.ShowDialog();
+      var dialog = new FolderBrowserDialog();
+      DialogResult result = dialog.ShowDialog();
       //DialogResult result = dlg.ShowDialog(this.GetIWin32Window());
       if (result == System.Windows.Forms.DialogResult.OK)
-        SourceDirectory.Text = dlg.SelectedPath;
+      {
+        SourceDirectory.Text = dialog.SelectedPath;
+        AddOrUpdateBaseInstallation();
+      }
+
+      CreateNewInstall.IsEnabled = HasBaseInstall;
     }
 
     private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
     {     
-      File.WriteAllText(INSTALLATIONS_JSON, JsonConvert.SerializeObject(new XvTInstall(BASE_GAME, SourceDirectory.Text)));
+      File.WriteAllText(INSTALLATIONS_JSON, JsonConvert.SerializeObject(Installations, Formatting.Indented));
     }
 
     private void CreateNewInstall_Click(object sender, RoutedEventArgs e)
     {
       var dialog = new NewInstall();
+      dialog.Owner = this;
       var result = dialog.ShowDialog() ?? false;
-      
+
       if (result)
       {
-        var newInstall = new XvTInstall(dialog.NewInstallName.Text, $"{SourceDirectory.Text} ({dialog.NewInstallName.Text})");
-        
-      }
+        Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait;
+        var newInstallPath = Path.GetFullPath($"{SourceDirectory.Text}/../{STAR_WARS_XVT} ({dialog.NewInstallName.Text})");
+        var sourceDirectory = SourceDirectory.Text;
 
+        CopyDirectory.IO.CopyDirectory(sourceDirectory, newInstallPath, true);
+        Installations.Add(new XvTInstall(dialog.NewInstallName.Text, newInstallPath));
+        Mouse.OverrideCursor = null;
+      }
     }
   }
 }
